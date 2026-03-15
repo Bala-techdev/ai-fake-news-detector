7aa2c4444be35101bc11c0a0368c9cf124845b9a
Balabala275 <balatkm2007@gmail.com>
2026-03-15 09:13:30 +0530

    adding file

---
+++ ai-fake-news-detector/ai-fake-news-detector/src/main/java/com/bala/ai_fake_news_detector/AiFakeNewsDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.bala.ai_fake_news_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFakeNewsDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFakeNewsDetectorApplication.class, args);
+	}
+
+}
---
+++ ai-fake-news-detector/ai-fake-news-detector/src/test/java/com/bala/ai_fake_news_detector/AiFakeNewsDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.ai_fake_news_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFakeNewsDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
